f0ad69327e05e51c8d756491101aeca064258cd7
VincentVibhavaPranata <vincentnoob78@gmail.com>
2025-06-03 16:37:36 +0700

    kimmy suka rehan

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('babii Dashboard') }}
+            {{ __('adminn Dashboard') }}
         </h2>
     </x-slot>
 
